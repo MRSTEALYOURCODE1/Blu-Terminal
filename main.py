9ce352eed3450576dbadf44c79401e3034d9cb48
Amitoj Singh Saini <69375705+MRSTEALYOURCODE1@users.noreply.github.com>
2020-12-26 19:21:10 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env
 
+import code
+import os
+
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+
+
 import datetime
 version = "1.0"
 
@@ -35,13 +43,124 @@ def arguments(user_input):
         now = datetime.datetime.now()
         print("Blu Closed On:")
         print (now.strftime("%Y-%m-%d %H:%M:%S"))
+        print(Fore.GREEN + "[Process Completed]")
         quit()
 
     if user_input == "keys":
         print("Keys-Are:")
+        inputs(version)
+
+    if user_input == "time" or user_input == "Time" or user_input == "TIME":
+        now = datetime.datetime.now()
+        print (now.strftime("%Y-%m-%d %H:%M:%S"))
+        inputs(version)
+
+    if user_input == "text" or user_input == "editor" or user_input == "Text" or user_input == "TEXT":
+        os.system('python3 more/code.py')
+        print("Your Back!!")
+        inputs(version)
+
+    if user_input == "calculator" or user_input == "-c-" or user_input == "Calculator" or user_input == "CALCULATOR":
+        while True:
+            num1 = input("Calculator 1.0$ ")
+
+            check = num1.isnumeric()
+
+            while check == False:
+                if num1 == "":
+                    break
+
+                if op == "q" or op == "quit" or op == "Quit" or op == "QUIT" or op == "Q":
+                    break
+                    
+                print("Error Bad Input")
+                num1 = input("Calculator 1.0$ ")
+                check = num1.isnumeric()
+
+            while num1 == "":
+                num1 = input("Calculator 1.0$ ")
+
+            if num1 == "done" or num1 == "q" or num1 == "quit":
+                inputs(version)
+                exit()
+
+            num1 = int(num1)
+
+            
+
+            
+
+            
+
+            op = input("Calculator 1.0$ ")
+
+            while op != "x" or op != "+" or op != "/" or op != "-" or op != "÷":
+                if op == "":
+                    break
+                if op == "x" or op == "+" or op == "/" or op == "-" or op == "÷":
+                    break
+                if op == "q" or op == "quit" or op == "Quit" or op == "QUIT" or op == "Q":
+                    break
+                print("Error Bad Operator")
+                op = input("Calculator 1.0$ ")
+
+            while op == "":
+                op = input("Calculator 1.0$ ")
+
+            if op == "done" or op == "q" or op == "quit":
+                inputs(version)
+                exit()
+
+            num2 = input("Calculator 1.0$ ")
+
+            check2 = num2.isnumeric()
+
+            while check2 == False:
+                if num2 == "":
+                    break
+
+                if num2 == "q" or op == "quit" or op == "Quit" or op == "QUIT" or op == "Q":
+                    break
+                
+                print("Error Bad Input")
+                num2 = input("Calculator 1.0$ ")
+                check = num2.isnumeric()
+
+            while num2 == "":
+                num2 = input("Calculator 1.0$ ")
+
+            if num2 == "done" or num2 == "q" or num2 == "quit":
+                inputs(version)
+                exit()
+
+
+            num2 = int(num2)
+
+            if op == "x":
+                print(num1 * num2)
+                inputs(version)
+                exit()
+            elif op == "+":
+                print(num1 + num2)
+                inputs(version)
+                exit()
+            elif op == "/" or op == "÷":
+                print(num1 / num2)
+                inputs(version)
+                exit()
+            elif op == "-":
+                print(num1 - num2)
+                inputs(version)
+                exit()
+            
+
+            
+        
+
 
     if user_input == "Amitoj":
         print("You Have Found The Secret Name Of Whom Made This BootLeg Bash")
+        inputs(version)
 
     if user_input == "download":
         print("Download What?")
